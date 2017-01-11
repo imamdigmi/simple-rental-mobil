9590a25d340a420def610945fc81ef7e57235650
imam digmi <imam.digmi@gmail.com>
2017-01-12 00:01:14 +0700

    MODIFY: home

--- pelanggan/home.php
+++ pelanggan/home.php
@@ -2,13 +2,25 @@
 	<div class="page-header">
 		<h2>Mobil <small>data mobil!</small></h2>
 	</div>
-	<?php $query = $connection->query("SELECT * FROM mobil"); while ($row = $query->fetch_assoc()): ?>
-		<div class="col-xs-6 col-md-3">
-	    <a href="assets/img/<?=$row['gambar']?>" class="thumbnail fancybox">
-	      <img src="assets/img/<?=$row['gambar']?>" alt="<?=$row['judul']?>">
-	    </a>
-	  </div>
-	<?php endwhile; ?>
+	<div class="row">
+		<?php $query = $connection->query("SELECT * FROM mobil"); while ($row = $query->fetch_assoc()): ?>
+			<div class="col-xs-6 col-md-3">
+				<div class="thumbnail">
+					<a href="assets/img/<?=$row['gambar']?>" class="fancybox">
+			      <img src="assets/img/<?=$row['gambar']?>" alt="<?=$row['judul']?>">
+			    </a>
+		      <div class="caption text-center">
+		        <h4><?=$row["nama_mobil"]?></h4>
+		        <h5>Rp.<?=$row["harga"]?>,-</h5>
+						<span class="label label-<?=($row['status']) ? "success" : "danger" ?>"><?=($row['status']) ? "Tersedia" : "Tidak Tersedia" ?></span>
+		        <p><br>
+							<a href="#" class="btn btn-primary">Sewa Sekarang!</a>
+						</p>
+		      </div>
+		    </div>
+		  </div>
+		<?php endwhile; ?>
+	</div>
 </div>
 
 <script type="text/javascript">

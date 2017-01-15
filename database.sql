d60d3ab19c9224cb011c7535fd4c92955dfd0a38
imam digmi <imam.digmi@gmail.com>
2017-01-16 02:31:59 +0700

    MODIFY: database

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 12, 2017 at 02:57 AM
+-- Generation Time: Jan 16, 2017 at 02:30 AM
 -- Server version: 5.7.16-0ubuntu0.16.10.1
 -- PHP Version: 7.0.13-0ubuntu0.16.10.1
 
@@ -54,9 +54,17 @@ INSERT INTO `admin` (`id_admin`, `nama`, `email`, `alamat`, `telp`, `username`, 
 CREATE TABLE `detail_transaksi` (
   `id_detail` int(11) NOT NULL,
   `id_transaksi` int(11) NOT NULL,
-  `id_mobil` int(11) NOT NULL,
-  `id_supir` int(11) NOT NULL
+  `id_supir` int(11) NOT NULL,
+  `jasa_supir` int(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_detail`, `id_transaksi`, `id_supir`, `jasa_supir`) VALUES
+(4, 7, 2, 30000),
+(5, 11, 2, 30000);
 
 -- --------------------------------------------------------
 
@@ -95,7 +103,7 @@ CREATE TABLE `konfirmasi` (
 --
 
 INSERT INTO `konfirmasi` (`id_konfirmasi`, `id_transaksi`, `bukti`) VALUES
-(2, 13, '212012017024823.png');
+(2, 11, '415012017235555.png');
 
 -- --------------------------------------------------------
 
@@ -119,8 +127,11 @@ CREATE TABLE `mobil` (
 --
 
 INSERT INTO `mobil` (`id_mobil`, `id_jenis`, `no_mobil`, `merk`, `nama_mobil`, `gambar`, `harga`, `status`) VALUES
-(3, 4, 'AB 1817 XY', 'Toyota', 'BMW FX 1', '11012017235401.png', 20000, '0'),
-(4, 5, 'AB 1817 XY', 'Toyota', 'BMW FX 1', '12012017000453.png', 20000, '0');
+(1, 3, 'Sedan', 'Sedan Lagi', 'Sedan Juga', '15012017231222.png', 900000, '1'),
+(2, 4, 'ab 3355 yk', 'hoon', 'haha', '15012017231548.png', 250000, '0'),
+(3, 5, 'ab 1717 bh', 'yam', 'yuhu', '15012017231638.png', 100000, '0'),
+(4, 4, 'ab 1768 wk', 'jjjj', 'yii', '15012017231736.png', 150000, '0'),
+(5, 3, 'ab 6767 sd', 'hon', 'hhhjh', '15012017231817.png', 200000, '1');
 
 -- --------------------------------------------------------
 
@@ -144,7 +155,8 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `no_ktp`, `nama`, `email`, `no_telp`, `alamat`, `username`, `password`) VALUES
-(2, '0123782137897', 'imam', 'imam.digmi@gmail.com', '08970008875', 'jogja', 'imam', '202cb962ac59075b964b07152d234b70');
+(4, '09810237127', 'pelanggan', 'pelanggan@gmail.com', '08970008875', 'jogja', 'pelanggan', '7f78f06d2d1262a0a222ca9834b15d9d'),
+(5, '012398120938', 'imam', 'imam.digmi@gmail.com', '08970008875', 'jogja', 'imam', 'eaccb8ea6090a40a98aa28c071810371');
 
 -- --------------------------------------------------------
 
@@ -165,7 +177,7 @@ CREATE TABLE `supir` (
 --
 
 INSERT INTO `supir` (`id_supir`, `nama`, `telp`, `alamat`, `status`) VALUES
-(2, 'imam', '08970008875', 'jogja', '0');
+(2, 'imam', '08970008875', 'jogja', '1');
 
 -- --------------------------------------------------------
 
@@ -176,23 +188,33 @@ INSERT INTO `supir` (`id_supir`, `nama`, `telp`, `alamat`, `status`) VALUES
 CREATE TABLE `transaksi` (
   `id_transaksi` int(11) NOT NULL,
   `id_pelanggan` int(11) NOT NULL,
+  `id_mobil` int(11) NOT NULL,
   `tgl_sewa` datetime NOT NULL,
+  `tgl_ambil` datetime DEFAULT NULL,
   `tgl_kembali` datetime DEFAULT NULL,
   `lama` tinyint(1) NOT NULL,
   `total_harga` int(7) NOT NULL,
   `status` enum('0','1') NOT NULL,
   `jaminan` varchar(30) NOT NULL,
-  `denda` int(7) DEFAULT NULL,
+  `denda` int(7) DEFAULT '0',
   `jatuh_tempo` datetime NOT NULL,
-  `konfirmasi` enum('0','1') DEFAULT NULL
+  `konfirmasi` enum('0','1') DEFAULT NULL,
+  `pembatalan` enum('0','1') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`id_transaksi`, `id_pelanggan`, `tgl_sewa`, `tgl_kembali`, `lama`, `total_harga`, `status`, `jaminan`, `denda`, `jatuh_tempo`, `konfirmasi`) VALUES
-(13, 2, '2017-01-12 02:12:03', NULL, 1, 50000, '0', 'STNK', NULL, '2017-01-13 02:12:03', '1');
+INSERT INTO `transaksi` (`id_transaksi`, `id_pelanggan`, `id_mobil`, `tgl_sewa`, `tgl_ambil`, `tgl_kembali`, `lama`, `total_harga`, `status`, `jaminan`, `denda`, `jatuh_tempo`, `konfirmasi`, `pembatalan`) VALUES
+(11, 4, 1, '2017-01-12 22:00:00', '2017-01-15 23:00:00', '2017-01-16 02:00:00', 1, 930000, '1', 'STNK', 105000, '2017-01-16 23:26:54', '1', '0'),
+(12, 4, 2, '2017-01-16 23:26:54', NULL, NULL, 1, 250000, '0', 'STNK', NULL, '2017-01-16 23:30:13', '0', '0'),
+(13, 4, 3, '2017-01-16 23:26:54', NULL, NULL, 1, 100000, '0', 'STNK', NULL, '2017-01-16 23:30:32', '0', '0'),
+(14, 4, 4, '2017-01-16 23:26:54', NULL, NULL, 1, 150000, '0', 'STNK', NULL, '2017-01-16 23:54:53', '0', '0'),
+(15, 4, 4, '2017-01-16 23:26:54', NULL, NULL, 1, 150000, '0', 'STNK', NULL, '2017-01-16 23:54:53', '0', '0'),
+(16, 4, 4, '2017-01-16 23:26:54', NULL, NULL, 1, 150000, '0', 'STNK', NULL, '2017-01-16 23:54:53', '0', '0'),
+(17, 4, 2, '2017-01-16 23:26:54', NULL, NULL, 1, 250000, '0', 'STNK', NULL, '2017-01-16 23:30:13', '0', '0'),
+(18, 4, 1, '2017-01-16 23:26:54', NULL, NULL, 1, 930000, '0', 'STNK', NULL, '2017-01-16 23:26:54', '1', '0');
 
 --
 -- Indexes for dumped tables
@@ -210,7 +232,6 @@ ALTER TABLE `admin`
 ALTER TABLE `detail_transaksi`
   ADD PRIMARY KEY (`id_detail`),
   ADD KEY `fk_transaksi` (`id_transaksi`),
-  ADD KEY `fk_mobil` (`id_mobil`),
   ADD KEY `fk_supir` (`id_supir`);
 
 --
@@ -250,7 +271,8 @@ ALTER TABLE `supir`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `fk_pelanggan` (`id_pelanggan`);
+  ADD KEY `fk_pelanggan` (`id_pelanggan`),
+  ADD KEY `fk_mobil` (`id_mobil`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -260,12 +282,12 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_admin` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `jenis`
 --
@@ -280,12 +302,12 @@ ALTER TABLE `konfirmasi`
 -- AUTO_INCREMENT for table `mobil`
 --
 ALTER TABLE `mobil`
-  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `supir`
 --
@@ -295,7 +317,7 @@ ALTER TABLE `supir`
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- Constraints for dumped tables
 --
@@ -305,7 +327,6 @@ ALTER TABLE `transaksi`
 --
 ALTER TABLE `detail_transaksi`
   ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_mobil`) REFERENCES `mobil` (`id_mobil`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `detail_transaksi_ibfk_3` FOREIGN KEY (`id_supir`) REFERENCES `supir` (`id_supir`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
@@ -324,7 +345,8 @@ ALTER TABLE `mobil`
 -- Constraints for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_mobil`) REFERENCES `mobil` (`id_mobil`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
